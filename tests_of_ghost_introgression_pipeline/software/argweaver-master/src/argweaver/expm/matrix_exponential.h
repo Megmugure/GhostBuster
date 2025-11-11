00d8f5d1e14e302b27cf2d09baffadc4c0610ae6
“Margaret <margaretwmugure@gmail.com>
2025-11-10 16:23:50 -0800

    Stop tracking large/derived dirs; add .gitignore

--- tests_of_ghost_introgression_pipeline/software/argweaver-master/src/argweaver/expm/matrix_exponential.h
+++
@@ -1,8 +0,0 @@
-
-extern "C" {
-
-double *expm11 ( int n, double a[] );
-double *expm2 ( int n, double a[] );
-double *expm3 ( int n, double a[] );
-
-}
--- tests_of_ghost_introgression_pipeline/software/argweaver-master/src/argweaver/fs.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARGWEAVER_FS_H
-#define ARGWEAVER_FS_H
-
-bool makedirs(const char *path, mode_t mode=0777);
-
-#endif
